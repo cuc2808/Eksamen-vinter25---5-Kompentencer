ad4c0f5832bdf166dccd6c06489094cf6c6fa952
cuc2808 <cucogco@gmail.com>
2026-01-04 00:03:30 +0100

    Finished Team methods. Added stuff in teamManager. Added UI. Need to add final stuff for use interaction.

--- src/Team.java
+++ src/Team.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Team {
 
     String teamName;
     ArrayList<Developer> developers = new ArrayList<>();
 
-    public Team(String teamName, ArrayList<Developer> developers) {
+    public Team(String teamName) {
         this.teamName = teamName;
-        this.developers = developers;
     }
 
     public void addDeveloper(String developerName) {
@@ -38,38 +36,109 @@ public class Team {
     }
 
     public Developer findDeveloper(String developerName) {
-        Developer developer = null;
+        Developer developer = new Developer("unknown");
         for (Developer developerInList : developers) {
             if (developerInList.getName().equalsIgnoreCase(developerName)) {
                 developer = developerInList;
             } else {
-                return new Developer("unknown");
+                return developer;
             }
         }
         return developer;
     }
 
     public void printTeamOverview() {
-
-    }
-
-    public TreeMap<Integer,Developer> rankDevelopersForSkill(String skillName) {
-        TreeMap<Integer,Developer> skillsRankedList = new TreeMap<>();
-
+        System.out.println("*========= Overview of Team: " + teamName.toUpperCase() + " =========*\n");
         for (Developer developer : developers) {
-            int skillLevel = 0;
+            System.out.println("Developer: " + developer.getName());
+            System.out.println("Skillset:");
             for (Skill skill : developer.skills) {
-                if(skill.getName().equalsIgnoreCase(skillName)) {
-                    skillLevel = skill.getLevel();
+                System.out.println(" - " + skill.getName() + " LVL: " + skill.getLevel());
+            }
+            System.out.println("\n------------------------------\n");
+        }
+    }
+//
+//    public TreeMap<Double, Developer> rankDevelopersForSkill(String skillName) {
+//        TreeMap<Double, Developer> skillsRankedList = new TreeMap<>();
+//
+//        for (Developer developer : developers) {
+//            double skillLevel = 0;
+//            for (Skill skill : developer.skills) {
+//                if (skill.getName().equalsIgnoreCase(skillName)) {
+//                    skillLevel = skill.getLevel();
+//                }
+//            }
+//            if (!skillsRankedList.containsKey(skillLevel)) {
+//                skillsRankedList.put(skillLevel, developer);
+//            } else if (skillsRankedList.containsKey(skillLevel)) {
+//                while(skillsRankedList.containsKey(skillLevel)) {
+//                    skillLevel += 0.01;
+//                }
+//
+//            }
+//        }
+//        return skillsRankedList;
+//    }
+
+    public LinkedHashMap<Developer, Integer> rankDevelopersForSkill(String skillName) {
+        ArrayList<Integer> skillsRankedList = new ArrayList<>();
+        LinkedHashMap<Developer, Integer> DevelopersRankedBySkill = new LinkedHashMap<>();
+
+        for (int skillLevel = 1; skillLevel <= 10; skillLevel++) {
+            skillsRankedList.add(skillLevel);
+        }
+        Collections.reverse(skillsRankedList);
+
+        for (Integer skillLevel : skillsRankedList) {
+            for (Developer developer : developers) {
+                for (Skill skill : developer.getSkills()) {
+                    if (skill.getName().equalsIgnoreCase(skillName) && skill.getLevel() == skillLevel) {
+                        DevelopersRankedBySkill.put(developer, skill.getLevel());
+                    }
                 }
             }
-            skillsRankedList.put(skillLevel,developer);
         }
-        System.out.println();
+
+        return DevelopersRankedBySkill;
     }
 
-    public ArrayList<Developer> findDevelopersMeetingRequirements() {
 
-        return null;
+    public void bestTopXDevelopersForSkill(String skillName, int amountShown) {
+        LinkedHashMap<Developer, Integer> developersRankedList = rankDevelopersForSkill(skillName);
+        System.out.println("Top " + amountShown + " list of developers ranked by the skill: " + skillName.toUpperCase() + "\n");
+        for (int i = 1; i <= amountShown; i++) {
+            String topDeveloperName = developersRankedList.firstEntry().getKey().getName();
+            int topDeveloperLVL = developersRankedList.firstEntry().getValue();
+            System.out.println(i + ". " + topDeveloperName + "LVL: " + topDeveloperLVL);
+            developersRankedList.remove(developersRankedList.firstEntry().getKey());
+        }
+        System.out.println("==================================");
+    }
+
+    public void bestDeveloperForSkill(String skillName) {
+        LinkedHashMap<Developer, Integer> developersRankedList = rankDevelopersForSkill(skillName);
+        String topDeveloperName = developersRankedList.firstEntry().getKey().getName();
+        int topDeveloperLVL = developersRankedList.firstEntry().getValue();
+        System.out.println("Most qualified:");
+        developersRankedList.keySet().forEach((Developer developer) -> {
+            if (topDeveloperLVL == developersRankedList.get(developer)) {
+                System.out.println(" - " + developer.getName());
+            }
+        });
+
+    }
+
+    public void findDevelopersMeetingRequirements(String skillName, int lvlRequirement) {
+        LinkedHashMap<Developer, Integer> developersRankedList = rankDevelopersForSkill(skillName);
+        System.out.println("List of developers that meet the requirements:");
+        System.out.println(("=== ranked by skill level === " + "\n"));
+        while (!developersRankedList.isEmpty()) {
+            String topDeveloperName = developersRankedList.firstEntry().getKey().getName();
+            int topDeveloperLVL = developersRankedList.firstEntry().getValue();
+            System.out.println(" - " + topDeveloperName + "LVL: " + topDeveloperLVL);
+            developersRankedList.remove(developersRankedList.firstEntry().getKey());
+        }
+        System.out.println("==================================");
     }
 }
--- src/TeamManager.java
+++ src/TeamManager.java
@@ -1,2 +1,87 @@
+import java.util.ArrayList;
+
 public class TeamManager {
+
+    public TeamManager() {
+        loadTeams();
+
+
+    }
+
+
+    public void loadTeams() {
+
+        Team redVelvets = new Team("Red Velvets");
+
+        redVelvets.addDeveloper("Jonas");
+        redVelvets.developers.get(0).addSkill("GitHub", 7);
+        redVelvets.developers.get(0).addSkill("Java", 7);
+        redVelvets.developers.get(0).addSkill("SQL", 7);
+        redVelvets.developers.get(0).addSkill("IRL Specialist", 7);
+
+        redVelvets.addDeveloper("Christopher");
+        redVelvets.developers.get(1).addSkill("GitHub", 2);
+        redVelvets.developers.get(1).addSkill("Java", 10);
+        redVelvets.developers.get(1).addSkill("SQL", 2);
+        redVelvets.developers.get(1).addSkill("IRL Specialist", 10);
+
+        redVelvets.addDeveloper("Arthur");
+        redVelvets.developers.get(2).addSkill("GitHub", 10);
+        redVelvets.developers.get(2).addSkill("Java", 1);
+        redVelvets.developers.get(2).addSkill("SQL", 1);
+        redVelvets.developers.get(2).addSkill("IRL Specialist", 10);
+
+        redVelvets.addDeveloper("Benjamin");
+        redVelvets.developers.get(3).addSkill("GitHub", 4);
+        redVelvets.developers.get(3).addSkill("Java", 8);
+        redVelvets.developers.get(3).addSkill("SQL", 10);
+        redVelvets.developers.get(3).addSkill("IRL Specialist", 9);
+
+        redVelvets.addDeveloper("Joachim");
+        redVelvets.developers.get(4).addSkill("GitHub", 5);
+        redVelvets.developers.get(4).addSkill("Java", 1);
+        redVelvets.developers.get(4).addSkill("SQL", 5);
+        redVelvets.developers.get(4).addSkill("IRL Specialist", 7);
+
+        redVelvets.printTeamOverview();
+
+        //==================================================
+
+        Team blueBirds = new Team("Blue Birds");
+
+        blueBirds.addDeveloper("Mads");
+        blueBirds.developers.get(0).addSkill("GitHub", 5);
+        blueBirds.developers.get(0).addSkill("Java", 5);
+        blueBirds.developers.get(0).addSkill("SQL", 5);
+        blueBirds.developers.get(0).addSkill("IRL Specialist", 9);
+
+        blueBirds.addDeveloper("Bertram");
+        blueBirds.developers.get(1).addSkill("GitHub", 5);
+        blueBirds.developers.get(1).addSkill("Java", 3);
+        blueBirds.developers.get(1).addSkill("SQL", 2);
+        blueBirds.developers.get(1).addSkill("IRL Specialist", 10);
+
+        blueBirds.addDeveloper("Patrick");
+        blueBirds.developers.get(2).addSkill("GitHub", 9);
+        blueBirds.developers.get(2).addSkill("Java", 9);
+        blueBirds.developers.get(2).addSkill("SQL", 4);
+        blueBirds.developers.get(2).addSkill("IRL Specialist", 1);
+
+        blueBirds.addDeveloper("Marko");
+        blueBirds.developers.get(3).addSkill("GitHub", 4);
+        blueBirds.developers.get(3).addSkill("Java", 4);
+        blueBirds.developers.get(3).addSkill("SQL", 4);
+        blueBirds.developers.get(3).addSkill("IRL Specialist", 4);
+
+        blueBirds.addDeveloper("Emil");
+        blueBirds.developers.get(4).addSkill("GitHub", 5);
+        blueBirds.developers.get(4).addSkill("Java", 1);
+        blueBirds.developers.get(4).addSkill("SQL", 10);
+        blueBirds.developers.get(4).addSkill("IRL Specialist", 5);
+
+        blueBirds.printTeamOverview();
+
+    }
+
+    public void team
 }
